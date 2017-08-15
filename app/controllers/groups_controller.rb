652e045ef6a206063c8186d86d266b26fa71944d
王涛 <wangtao@wangtaodeMacBook-Pro.local>
2017-08-15 18:19:29 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def new
@@ -16,7 +17,7 @@ class GroupsController < ApplicationController
 
   def create
     @group=Group.new(group_params)
-    @group.user=current_user
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
